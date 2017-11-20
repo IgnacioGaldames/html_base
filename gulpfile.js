0441c69b9455793fa437d5d6539f06feb9f20d74
Ignacio Galdames <caminantestelar@gmail.com>
2017-11-20 17:31:56 -0300

    iniciar browsewrsync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,46 @@
+//Requeridos
 var gulp = require('gulp');
+var sass = require('gulp-sass'); //  the gulp-sass plugin
+var browserSync = require('browser-sync').create(); //Instalar Browsersync
 
-// Requires the gulp-sass plugin
-var sass = require('gulp-sass');
+//Nombres de carpetas
+var source = '_source';
+var staging = '_staging';
+var distribuition = '_dist'
 
+//Task de ejemplo
 gulp.task('hello', function() {
     console.log('Hello World');
   });
 
-gulp.task('sass', function(){
-    return gulp.src('_source/_sass/test.sass')
+/*
+gulp.task('watch', ['array', 'of', 'tasks', 'to', 'complete','before', 'watch'], function (){
+    // ...
+  })
+*/
+
+// Iniciar el compilador SASS
+gulp.task('compilador-sass', function(){
+    return gulp.src( source + '/_sass/test.sass')
       .pipe(sass()) // Converts Sass to CSS with gulp-sass
-      .pipe(gulp.dest('_staging/css'))
+      .pipe(gulp.dest( staging + '/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
 });
 
-gulp.watch('_source/_sass/**/*.+(scss|sass)', ['sass']);
+//Iniciar Browsersync
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: staging 
+    },
+  })
+})
+
+//Concatenar tasks
+
+gulp.task('watch', ['browserSync', 'compilador-sass'], function (){
+  gulp.watch( source + '/_sass/**/*.+(scss|sass)', ['compilador-sass']);
+  // Other watchers
+})
