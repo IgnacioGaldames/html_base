afff927274cb47e52888058ed90ea4f91214d6fd
Ignacio Galdames <caminantestelar@gmail.com>
2017-11-20 17:57:41 -0300

    añadir task jekyll

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 //Requeridos
-var gulp = require('gulp');
-var sass = require('gulp-sass'); //  the gulp-sass plugin
-var browserSync = require('browser-sync').create(); //Instalar Browsersync
+const gulp = require('gulp');
+const sass = require('gulp-sass'); //  the gulp-sass plugin
+const browserSync = require('browser-sync').create(); //Instalar Browsersync
+const sourcemaps = require('gulp-sourcemaps'); //Añadir sourcemaps a sass
+const child = require('child_process');
 
 //Nombres de carpetas
-var source = '_source';
-var staging = '_staging';
-var distribuition = '_dist'
+const source = '_source';
+const staging = '_staging';
+const distribuition = '_dist'
 
 //Task de ejemplo
 gulp.task('hello', function() {
@@ -22,10 +24,21 @@ gulp.task('watch', ['array', 'of', 'tasks', 'to', 'complete','before', 'watch'],
 // Iniciar el compilador SASS
 gulp.task('compilador-sass', function(){
     return gulp.src( source + '/_sass/test.sass')
-      .pipe(sass()) // Converts Sass to CSS with gulp-sass
+      .pipe(sourcemaps.init())
+      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+      .pipe(sourcemaps.write('./')) 
       .pipe(gulp.dest( staging + '/css'))
       .pipe(browserSync.reload({
         stream: true
+      }))
+});
+
+// Jekyll Task
+gulp.task('jekyll', function() {
+  const jekyll = child.spawn('jekyll', ['build','--watch', '--incremental'])
+  return gulp.src(source)
+      .pipe(browserSync.reload({
+          stream: true
       }))
 });
 
@@ -43,4 +56,6 @@ gulp.task('browserSync', function() {
 gulp.task('watch', ['browserSync', 'compilador-sass'], function (){
   gulp.watch( source + '/_sass/**/*.+(scss|sass)', ['compilador-sass']);
   // Other watchers
+  gulp.watch( source + '/*.html', browserSync.reload); 
+  gulp.watch( source + '/js/**/*.js', browserSync.reload); 
 })
