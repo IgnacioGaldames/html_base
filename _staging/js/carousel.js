346c3162ac3a2a3c077cf2dd7d9b52326fa87b6c
Ignacio Galdames <caminantestelar@gmail.com>
2017-12-04 14:53:54 -0300

    custom bootstrap

--- _staging/js/carousel.js
+++ _staging/js/carousel.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
-        lazyLoad: 'true',
-        autoplay: 'false',
-        transitionStyle: 'fade',
-        autoplayHoverPause: 'true',
-        nav: 'true',
-        dots: 'true',
-        items: '1'
+    $(".slider").owlCarousel({
+        autoPlay: 5000,
+        items: 1,
+        itemsDesktop: [1214, 1],
+        itemsDesktopSmall: [994, 1],
+        itemsTablet: [543, 1],
+        itemsMobile: 1,
+        autoPlay: true,
+        lazyLoad: true,
+        navigationText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        navigation: true,
+        autoHeight: false,
+        pagination: false,
+        loop: true,
+    });
+    $(".slider-marcas").owlCarousel({
+        autoPlay: 5000,
+        items: 4,
+        itemsDesktop: [1214, 4],
+        itemsDesktopSmall: [994, 4],
+        itemsTablet: [543, 4],
+        itemsMobile: 4,
+        autoPlay: true,
+        lazyLoad: true,
+        navigationText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        navigation: true,
+        autoHeight: false,
+        pagination: false,
+        loop: true,
     });
 });
