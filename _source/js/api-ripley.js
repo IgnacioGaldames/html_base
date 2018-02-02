eccc73b2cc683d056fde995812c2e26cab569a7d
IgnacioGaldames-Cardumen <ignacio.galdames@cardumen.cl>
2018-02-02 11:26:01 -0300

    test animación frames / nueva api

--- _source/js/api-ripley.js
+++ _source/js/api-ripley.js
@@ -14,18 +14,32 @@ $(document).ready(function () {
                 // Valores obtenidos de la URL
                 var idCorta = String(response.uniqueID);
                 var idLarga = String(response.partNumber);
+
                 var productoNombre = String(response.name);
                 var descripcionCorta = String(response.shortDescription);
                 var descripcionlarga = String(response.longDescription);
+
                 var urlProducto = String(response.url);
+
+                var precioNormal = String(response.prices.formattedListPrice);
+                var precioInternet = String(response.prices.formattedOfferPrice);
+                var precioOpex = String(response.prices.formattedCardPrice);
+                var totalDescuento = String(response.prices.formattedDiscount);
+
+                var porcentajeDescuento = parseInt(response.prices.discountPercentage);
+                var puntosRipley = parseInt(response.prices.ripleyPuntos);
+
                 var imagenThumbnail = String(response.thumbnailImage);
                 var imagenCompleta = String(response.fullImage);
-                var esMarketplace = Boolean(response.isMarketplaceProduct)
+
+                var esMarketplace = Boolean(response.isMarketplaceProduct);
+                var esSinStock = Boolean(response.isOutOfStock);
+                var esNoDisponible = Boolean(response.isUnavailable);
                 
                 
                 // Inyectar los datos obtenidos desde la url
                 productoDinamico.append(html);
-                console.log(esMarketplace);
+                console.log(idCorta,idLarga,productoNombre,descripcionCorta,descripcionlarga,urlProducto,imagenThumbnail,imagenCompleta,esMarketplace,esSinStock,esNoDisponible,precioNormal,precioInternet,precioOpex,totalDescuento,porcentajeDescuento,puntosRipley);
             },
             error: function () {
                 productoDinamico.remove();
